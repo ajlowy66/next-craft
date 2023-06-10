0e60ced988dff8b2b15aade598710c22033cc74b
Andrew Lowy <64696318+ajlowy66@users.noreply.github.com>
2023-06-11 00:16:46 +1000

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -7,7 +7,7 @@ const BLOG = {
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   craftConfigShareUrl: process.env.CRAFT_CONFIG_SHARE_URL
     ? process.env.CRAFT_CONFIG_SHARE_URL
-    : 'https://www.craft.do/s/Wofgn3dVj8wvET', // The link to share your craft config
+    : 'https://www.craft.do/s/XhGZhKbmAVgwBw', // The link to share your craft config
   seo: {
     keywords: ['Blog', 'Craft.do', 'Craft Docs', 'Next.js', 'TailwindCSS']
   },
